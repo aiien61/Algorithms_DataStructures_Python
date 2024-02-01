55d6fdaca5557d6c569d4c3c22f1b63d60fe93b6
aiien61 <aiien61will@gmail.com>
2024-02-01 14:56:31 +0800

    pretty table

--- pretty_print/pretty_table.py
+++ pretty_print/pretty_table.py
@@ -1,1 +1,14 @@
-import prettytable
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+pokemons = ["Pikachu", "Squirtle", "Charmander"]
+types = ["Electric", "Water", "Fire"]
+
+table.add_column("Pokemon Name", pokemons)
+table.add_column("Type", types)
+
+print(table)
+
+table.align = "l"
+print(table)
