f44e4952e51bad72d1a31caa79690ce5546bf350
aiien61 <aiien61will@gmail.com>
2024-02-18 15:50:31 +0800

    more pythonic way to implement binary search tree

--- Tree/Binary_Search_Tree/binary_search_tree_operations.py
+++ Tree/Binary_Search_Tree/binary_search_tree_operations.py
@@ -8,6 +8,127 @@ from dataclasses import dataclass
 from utils.treeviewer import plot_tree_graph
 
 
+class BSTNode:
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value: int) -> None:
+        if self.value is None:
+            self.value = value
+        
+        if value == self.value:
+            return None
+        elif value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BSTNode(value)
+        elif value > self.value:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
+        return None
+    
+    def search(self, value: int) -> object:
+        if not self:
+            return None
+
+        if value == self.value:
+            return self
+        elif value < self.value:
+            return self.left.search(value)
+        elif value > self.value:
+            return self.right.search(value)
+        return None
+    
+    def delete(self, value: int) -> object:
+        if not self:
+            return None
+        
+        if value == self.value:
+            if self.left is None:
+                self = self.right
+            elif self.right is None:
+                self = self.left
+            else:
+                left_max_node = self.left.get_max()
+                self.value = left_max_node.value
+                self.left = self.left.delete(left_max_node.value)
+        elif value < self.value:
+            if self.left:
+                self.left = self.left.delete(value)
+        elif value > self.value:
+            if self.right:
+                self.right = self.right.delete(value)
+        return self
+    
+    def get_min(self) -> object:
+        if not self.left:
+            return self
+        return self.left.get_min()
+    
+    def get_max(self) -> object:
+        if not self.right:
+            return self
+        return self.right.get_max()
+    
+    def preorder(self, order_by: str='left') -> None:
+        if not self:
+            return None
+
+        if order_by == "left":
+            print(self.value)
+            self.preorder(self.left, order_by)
+            self.preorder(self.right, order_by)
+        elif order_by == "right":
+            print(self.value)
+            self.preorder(self.right, order_by)
+            self.preorder(self.left, order_by)
+        
+        return None
+    
+    def inorder(self, order_by: str = 'left') -> None:
+        if not self:
+            return None
+
+        if order_by == "left":
+            self.inorder(self.left, order_by)
+            print(self.value)
+            self.inorder(self.right, order_by)
+        elif order_by == "right":
+            self.inorder(self.right, order_by)
+            print(self.value)
+            self.preorder(self.left, order_by)
+
+        return None
+    
+    def postorder(self, order_by: str = 'left') -> None:
+        if not self:
+            return None
+
+        if order_by == "left":
+            self.postorder(self.left, order_by)
+            print(self.value)
+            self.postorder(self.right, order_by)
+        elif order_by == "right":
+            self.postorder(self.right, order_by)
+            print(self.value)
+            self.postorder(self.left, order_by)
+
+        return None
+    
+
+def build_tree(root: BSTNode, array: Iterable) -> BSTNode:
+    for value in array:
+        if (value is None) or np.isnan(value):
+            continue
+        root.insert(value)
+    return root
+
+
 class BinaryTreeList:
     def __init__(self, array: Iterable):
         self.array = array
@@ -16,7 +137,7 @@ class BinaryTreeList:
 
     @dataclass
     class Node:
-        data: int
+        value: int
         times: int = 1
         left: object = None
         right: object = None
@@ -37,13 +158,13 @@ class BinaryTreeList:
         if node is None:
             return self.Node(value)
 
-        if value == node.data:
+        if value == node.value:
             node.times += 1
-        elif value < node.data:
+        elif value < node.value:
             pole_node = self._get_insert_position(node.left, value)
             if node.left is None:
                 node.left = pole_node
-        elif value > node.data:
+        elif value > node.value:
             pole_node = self._get_insert_position(node.right, value)
             if node.right is None:
                 node.right = pole_node
@@ -62,7 +183,7 @@ class BinaryTreeList:
         
         node_to_replace = node
 
-        if value == node.data:
+        if value == node.value:
             # X X
             if (node.left is None) and (node.right is None):
                 node_to_replace = None
@@ -79,11 +200,11 @@ class BinaryTreeList:
                 new_child_node = self._get_delete_position(node.right, value)
                 if node.right != new_child_node:
                     node.right = new_child_node
-        elif value < node.data:
+        elif value < node.value:
             new_child_node = self._get_delete_position(node.left, value)
             if node.left != new_child_node:
                 node.left = new_child_node
-        elif value > node.data:
+        elif value > node.value:
             new_child_node = self._get_delete_position(node.right, value)
             if node.right != new_child_node:
                 node.right = new_child_node
@@ -93,11 +214,11 @@ class BinaryTreeList:
     def search(self, value: int) -> object:
         node = self.root
         while node is not None:
-            if value == node.data:
+            if value == node.value:
                 return node
-            elif value < node.data:
+            elif value < node.value:
                 node = node.left
-            elif value > node.data:
+            elif value > node.value:
                 node = node.right
         
         if node is None:
@@ -129,7 +250,7 @@ class BinaryTreeList:
     
     @staticmethod
     def swap_node_data(node_x, node_y) -> None:
-        node_x.data, node_y.data = node_y.data, node_x.data
+        node_x.value, node_y.value = node_y.value, node_x.value
         return None
     
 
@@ -216,6 +337,14 @@ class BinaryTreeArray:
 
 
 if __name__ == "__main__":
+    # Binary Search Tree (Node)
+    numbers = np.array([5, 2, None, 7, 4, 8, 1, 10, 3, 7, 9, 2])
+    bst_node = BSTNode()
+    bst_node = build_tree(bst_node, numbers)
+    plot_tree_graph(tree_structure="list", to_file="node_tree_graph.png", tree_root=bst_node)
+
+
+
     # # Binary Search Tree (Linked List)
     # numbers = np.array([5, 2, 6, 1, 4, 7, 3])
     # bst = BinaryTreeList(numbers)
@@ -240,18 +369,18 @@ if __name__ == "__main__":
     # bst.delete(5)
     # plot_tree_graph(bst.root, "delete_root.png")
 
-    # Binary Search Tree (Array)
-    numbers = np.array([5, 2, None, 7, 4, 8, 1, 9, 3, 7, 10, 2])
-    bst = BinaryTreeArray(numbers)
-    print(bst)
+    # # Binary Search Tree (Array)
+    # numbers = np.array([5, 2, None, 7, 4, 8, 1, 9, 3, 7, 10, 2])
+    # bst = BinaryTreeArray(numbers)
+    # print(bst)
 
-    tree_args = {"tree_root_index": 0, "tree_array": bst.tree}
-    plot_tree_graph(tree_structure="array", to_file="arraytree.png", **tree_args)
+    # tree_args = {"tree_root_index": 0, "tree_array": bst.tree}
+    # plot_tree_graph(tree_structure="array", to_file="arraytree.png", **tree_args)
 
-    # search
-    print(f"search result: {bst.search(10)}")
-    print(f"search result: {bst.search(9)}")
-    try:
-        print(f"search result: {bst.search(11)}")
-    except ValueError:
-        print('Not Found')
+    # # search
+    # print(f"search result: {bst.search(10)}")
+    # print(f"search result: {bst.search(9)}")
+    # try:
+    #     print(f"search result: {bst.search(11)}")
+    # except ValueError:
+    #     print('Not Found')
