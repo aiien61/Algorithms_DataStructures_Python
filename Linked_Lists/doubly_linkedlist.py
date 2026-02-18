ea97478fcbd3d02d241a7038dc3a8804d9ff7c1b
aiien61 <aiien61will@gmail.com>
2026-02-19 01:46:46 +0800

    update

--- Linked_Lists/doubly_linkedlist.py
+++ Linked_Lists/doubly_linkedlist.py
@@ -1,132 +1,190 @@
-# TODO: deprecate this doubly linked list python implementation
-# TODO: use the testcase of this file to test simple_doubly_linkedlist.py
+from dataclasses import dataclass
+from typing import Self, Optional
 
+@dataclass
 class Node:
+    value: int
+    prev_node: Optional[Self] = None
+    next_node: Optional[Self] = None
 
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.prev = None
+@dataclass
+class DoublyLinkedList:
+    head: Optional[Node] = None
+    tail: Optional[Node] = None
+    length: int = 0
 
+    def __iter__(self):
+        temp = self.head
+        while temp:
+            yield temp
+            temp = temp.next_node
+    
+    def __str__(self):
+        return " <-> ".join([str(node.value) for node in self])
+    
+    def to_list(self) -> list[int]:
+        return [node.value for node in self]
+    
+    def append(self, value: int) -> bool:
+        new_node = Node(value)
 
-class LinkedList:
-
-    def __init__(self):
-        self.head = None
-        self._size = 0
-
-    def prepend(self, data):
-        self.size += 1
-        new_node = Node(data)
-        if self.head:
-            self.head.prev = new_node
-            new_node.next = self.head
-        self.head = new_node
-        return None
-
-    def append(self, data):
-        self.size += 1
-        new_node = Node(data)
-        node = self.head
-        while node.next is not None:
-            node = node.next
-        node.next = new_node
-        new_node.prev = node
-        return None
-
-    def insert(self, data, position):
-        self.size += 1
-
-        if position == 0:
-            self.prepend(data)
-            return None
-
-        new_node = Node(data)
-        current = self.head
-        prev = None
-        count = 0
-        while current is not None:
-            if count == position:
-                break
-            prev = current
-            current = current.next
-            count += 1
-
-        if current is not None:
-            new_node.next = current
-            current.prev = new_node
-        prev.next = new_node
-        new_node.prev = prev
-        return None
-
-    def remove(self, data):
         if not self.head:
-            return None
-
-        current = self.head
-        while current is not None:
-            if current.data == data:
-                break
-            current = current.next
-
-        if current is None:
-            return None
-
-        self.size -= 1
-        if current.prev is None:
-            current.next.prev = None
-            self.head = current.next
+            self.head = new_node
+            self.tail = new_node
         else:
-            current.prev.next = current.next
-            current.next.prev = current.prev
-        return None
+            self.tail.next_node = new_node
+            new_node.prev_node = self.tail
+            self.tail = new_node
+        
+        self.length += 1
+        return True
+    
+    def pop(self) -> Optional[Node]:
+        if self.length == 0:
+            return None
+        
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.tail = self.tail.prev_node
+            self.tail.next_node = None
+            temp.prev_node = None
 
-    def __repr__(self):
-        string = []
-        node = self.head
-        while node is not None:
-            string.append(node.data)
-            node = node.next
-        return str(string)
+        self.length -= 1
+        return temp
+    
+    def prepend(self, value: int) -> bool:
+        new_node = Node(value)
 
-    @property
-    def size(self):
-        return self._size
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next_node = self.head
+            self.head.prev_node = new_node
+            self.head = new_node
+        
+        self.length += 1
+        return True
+    
+    def pop_first(self) -> Optional[Node]:
+        if self.length == 0:
+            return None
+        
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next_node
+            self.head.prev_node = None
+            temp.next_node = None
 
-    @size.setter
-    def size(self, value):
-        self._size = value
+        self.length -= 1
+        return temp
+    
+    def get_node(self, index: int) -> Optional[Node]:
+        if index < 0 or self.length <= index:
+            return None
+        
+        if index < self.length / 2:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next_node
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev_node
 
+        return temp
+
+    def set_node(self, index: int, value: int) -> bool:
+        temp = self.get_node(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert(self, index: int, value: int) -> bool:
+        if index < 0 or self.length < index:
+            return False
+        
+        if index == 0:
+            return self.prepend(value)
+        
+        if index == self.length:
+            return self.append(value)
+        
+        new_node = Node(value)
+
+        after = self.get_node(index)
+        before = after.prev_node
+
+        new_node.prev_node = before
+        new_node.next_node = after
+
+        before.next_node = new_node
+        after.prev_node = new_node
+        
+        self.length += 1
+        return True
+
+    def remove(self, index: int) -> Optional[Node]:
+        if index < 0 or self.length <= index:
+            return None
+        
+        if index == 0:
+            return self.pop_first()
+        
+        if index == self.length - 1:
+            return self.pop()
+        
+        temp = self.get_node(index)
+
+        temp.next_node.prev_node = temp.prev_node
+        temp.prev_node.next_node = temp.next_node
+
+        temp.prev_node = None
+        temp.next_node = None
+
+        self.length -= 1
+        return temp
+
+def main():
+    dllist = DoublyLinkedList()
+    dllist.append(10)
+    dllist.append(20)
+    print(dllist)
+
+    print(dllist.pop())
+    print(dllist)
+    print(dllist.pop())
+    print(dllist)
+
+    dllist.prepend(10)
+    print(dllist)
+    dllist.prepend(20)
+    print(dllist)
+    dllist.prepend(15)
+    print(dllist)
+
+    print(dllist.pop_first())
+    print(dllist)
+
+    print(dllist.get_node(10))
+    print(dllist.get_node(1))
+    print(dllist.get_node(0))
+
+    dllist.set_node(0, 30)
+    print(dllist)
+
+    dllist.insert(1, 20)
+    print(dllist)
+
+    print(dllist.remove(1))
+    print(dllist)
 
 if __name__ == "__main__":
-    doubly_linked_list = LinkedList()
-    print(doubly_linked_list)
-    print(doubly_linked_list.size)
-    print()
-
-    doubly_linked_list.prepend(0)
-    print(doubly_linked_list)
-    print(doubly_linked_list.size)
-    print()
-
-    data = [20, 30, 50, 10, 90, 80, 70]
-    for _ in range(len(data)):
-        doubly_linked_list.append(data.pop())
-        print(doubly_linked_list)
-        print(doubly_linked_list.size)
-        print()
-
-    doubly_linked_list.insert(60, 3)
-    print(doubly_linked_list)
-    print(doubly_linked_list.size)
-    print()
-
-    doubly_linked_list.remove(90)
-    print(doubly_linked_list)
-    print(doubly_linked_list.size)
-    print()
-
-    doubly_linked_list.insert(60, 10)
-    print(doubly_linked_list)
-    print(doubly_linked_list.size)
-    print()
+    main()
--- Linked_Lists/singly_linkedlist.py
+++ Linked_Lists/singly_linkedlist.py
@@ -1,261 +1,219 @@
-# TODO: deprecate this singly linked list python implementation
-# TODO: use the testcase of this file to test simple_singly_linkedlist.py
+from dataclasses import dataclass
+from typing import Self, Optional
 
-# class LinkedList:
-
-#     def __init__(self):
-#         self.head = None
-#         self._size = 0
-
-
-#     def prepend(self, data):
-#         self.size += 1
-#         new_node = Node(data)
-#         if self.head:
-#             new_node.next = self.head
-#         self.head = new_node
-#         return None
-
-
-#     def append(self, data):
-#         self.size += 1
-#         node = self.head
-#         while node.next is not None:
-#             node = node.next
-#         node.next = Node(data)
-#         return None
-
-
-#     def insert(self, data, position):
-#         self.size += 1
-        
-#         if position == 0:
-#             self.prepend(data)
-#             return None
-
-#         new_node = Node(data)
-#         current = self.head
-#         prev = None
-#         count = 0
-        
-#         while current and count < position:
-#             prev = current
-#             current = current.next
-#             count += 1
-
-#         if count < position:
-#             prev.next = new_node
-#             return None
-        
-#         prev.next = new_node
-#         new_node.next = current
-#         return None
-
-#     def remove(self, data):
-#         if not self.head:
-#             return None
-
-#         current = self.head
-#         prev = None
-#         while current is not None:
-#             if current.data == data:
-#                 break
-#             prev = current
-#             current = current.next
-        
-#         if current is None:
-#             return None
-        
-#         self.size -= 1
-#         if prev is None:
-#             self.head = current.next
-#         else:
-#             prev.next = current.next
-#         return None
-
-#     def __repr__(self):
-#         string = []
-#         node = self.head
-#         while node is not None:
-#             string.append(node.data)
-#             node = node.next
-#         return str(string)
-
-
-#     @property
-#     def size(self):
-#         return self._size
-
-
-#     @size.setter
-#     def size(self, value):
-#         self._size = value
-
-
+@dataclass
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+    value: int
+    _next_node: Optional[Self] = None
+    
+    @property
+    def next_node(self) -> Optional[Self]:
+        return self._next_node
+    
+    @next_node.setter
+    def next_node(self, value: Self) -> None:
+        self._next_node = value
+        return None
 
-    def __repr__(self):
-        return f"node(data={self.data})"
-
-
+@dataclass
 class LinkedList:
+    head: Optional[Node] = None
+    tail: Optional[Node] = None
+    _length: int = 0
+
+    def __iter__(self):
+        temp = self.head
+        while temp:
+            yield temp
+            temp = temp.next_node
+
+    def __str__(self):
+        return " -> ".join([f'{node.value}' for node in self])
+
+    @property
+    def length(self) -> int:
+        return self._length
     
-    def __init__(self):
-        self.head = None
-        self.tail = None
+    def to_list(self) -> list[int]:
+        return [node.value for node in self]
 
-    
-    def __repr__(self):
-        linkedlist = []
-        node = self.head
-        while node is not None:
-            linkedlist.append(node.data)
-            node = node.next
-        return str(linkedlist)
+    def append(self, value: int) -> bool:
+        node = Node(value)
 
-
-    def append(self, data):
-        node = Node(data)
-        if not self.tail:
+        if not self.head:
             self.head = node
             self.tail = node
+        else:
+            self.tail.next_node = node
+            self.tail = node
+
+        self._length += 1
+        return True
+    
+    def prepend(self, value: int) -> bool:
+        node = Node(value)
+
+        if not self.head:
+            self.head = node
+            self.tail = node
+        else:
+            node.next_node = self.head
+            self.head = node
+        
+        self._length += 1
+        return True
+    
+    def pop(self) -> Optional[Node]:
+        if self._length == 0:
             return None
         
-        self.tail.next = node
-        self.tail = node
-        return None
-
-
-    def search(self, data):
-        if not self.tail:
-            return None
+        prev = self.head
+        temp = self.head
+        while temp.next_node:
+            prev = temp
+            temp = temp.next_node
         
-        node = self.head
-        while node:
-            if node.data == data:
-                return node
-            node = node.next
-        return None
-
-
-    def remove(self, data):
+        self.tail = prev
+        self.tail.next_node = None
+        self._length -= 1
+        if self._length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+    
+    def pop_first(self) -> Optional[Node]:
         if not self.head:
             return None
         
-        if self.head.data == data:
-            if self.head == self.tail:
-                self.head = self.tail = None
-            else:
-                self.head = self.head.next
+        temp = self.head
+        self.head = self.head.next_node
+        temp.next_node = None
+
+        self._length -= 1
+        if self._length == 0:
+            self.tail = None
+        
+        return temp
+    
+    def get_node(self, index: int) -> Optional[Node]:
+        if index < 0 or self._length <= index:
             return None
 
-        prev = self.head
-        current = self.head.next
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next_node
+        return temp
+    
+    def set_node(self, index: int, value: int) -> bool:
+        node = self.get_node(index)
+        if node is None:
+            return False
 
-        while current:
-            if current.data == data:
-                if current == self.tail:
-                    self.tail = prev
-
-                prev.next = current.next
-                return None
-            
-            prev = current
-            current = current.next
+        node.value = value
+        return True
+    
+    def insert(self, index: int, value: int) -> bool:
+        if index < 0 or self._length < index:
+            return False
         
-        return None
+        if index == 0:
+            return self.prepend(value)
+        
+        if index == self._length:
+            return self.append(value)
+        
+        node = Node(value)
+        
+        temp = self.get_node(index - 1)
+        node.next_node = temp.next_node
+        temp.next_node = node
+        self._length += 1
+        return True
+    
+    def remove(self, index: int) -> Optional[Node]:
+        if index < 0 or self._length <= index:
+            return None
+        
+        if index == 0:
+            return self.pop_first()
+        
+        if index == self._length - 1:
+            return self.pop()
+        
+        prev = self.get_node(index - 1)
+        temp = prev.next_node
+        prev.next_node = temp.next_node
+        temp.next_node = None
+        self._length -= 1
+        return temp
+    
+    def reverse(self):
+        temp = self.head
+        self.head, self.tail = self.tail, self.head
+
+        before = None
+        after = temp.next_node
+
+        for _ in range(self._length):
+            after = temp.next_node
+            temp.next_node = before
+            before = temp
+            temp = after
+        
+        return None  
+
+def main():
+    llist = LinkedList()
+    llist.append(3)
+    llist.append(5)
+    llist.append(10)
+    print(llist)
+
+    llist.pop()
+    print(llist)
+    llist.pop()
+    print(llist)
+    llist.pop()
+    print(llist)
+    print(llist.pop())
+
+    llist.prepend(10)
+    print(llist)
+    llist.prepend(3)
+    print(llist)
+
+    print(llist.pop_first())
+    print(llist)
+    print(llist.pop_first())
+    print(llist)
+    print(llist.pop_first())
+
+    print(llist.get_node(1))
+    llist.append(10)
+    llist.append(5)
+    llist.append(3)
+    print(llist)
+    print(llist.get_node(0))
+    print(llist.get_node(2))
+    llist.set_node(2, 20)
+    print(llist)
+
+    llist.insert(1, 30)
+    print(llist)
+
+    print(llist.remove(100))
+    print(llist.remove(0))
+    print(llist)
+    print(llist.remove(1))
+    print(llist)
+
+    llist.append(100)
+    print(llist)
+    llist.reverse()
+    print(llist)
+
+    print(llist.to_list())
+
 
 if __name__ == "__main__":
-    linked_list = LinkedList()
-    print(linked_list)
-
-    linked_list.append(9)
-    print(linked_list)
-    print(linked_list.head)
-    print(linked_list.tail)
-
-    linked_list.append(11)
-    linked_list.append(2)
-    linked_list.append(98)
-    linked_list.append(35)
-    print(linked_list)
-    print(linked_list.head)
-    print(linked_list.tail)
-
-    node = linked_list.search(98)
-    print(node)
-
-    linked_list.remove(2)
-    linked_list.remove(9)
-    linked_list.remove(35)
-    print(linked_list)
-    print(linked_list.head)
-    print(linked_list.tail)
-
-
-
-# if __name__ == "__main__":
-#     linked_list = LinkedList()
-#     print(linked_list)
-#     print(linked_list.size)
-#     print()
-
-#     print(f'prepend 20 to the head:')
-#     linked_list.prepend(20)
-#     print(linked_list)
-#     print(linked_list.size)
-#     print()
-
-#     print(f'append 30 to the tail:')
-#     linked_list.append(30)
-#     print(linked_list)
-#     print(linked_list.size)
-#     print()
-
-#     print(f'insert 0 to the position 1:')
-#     linked_list.insert(0, 1)
-#     print(linked_list)
-#     print(linked_list.size)
-#     print()
-
-#     print(f'insert 1 to the position 0:')
-#     linked_list.insert(1, 0)
-#     print(linked_list)
-#     print(linked_list.size)
-#     print()
-
-#     print(f'insert 100 to the position 2:')
-#     linked_list.insert(100, 2)
-#     print(linked_list)
-#     print(linked_list.size)
-#     print()
-
-#     print(f'append 200 to the tail:')
-#     linked_list.append(200)
-#     print(linked_list)
-#     print(linked_list.size)
-#     print()
-
-#     print(f'prepend 300 to the head:')
-#     linked_list.prepend(300)
-#     print(linked_list)
-#     print(linked_list.size)
-#     print()
-
-#     print(f'remove 10:')
-#     linked_list.remove(10)
-#     print(linked_list)
-#     print(linked_list.size)
-#     print()
-
-#     print(f'remove 100:')
-#     linked_list.remove(100)
-#     print(linked_list)
-#     print(linked_list.size)
-#     print()
-
-
-
+    main()
